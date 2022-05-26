1aad300334d5e71d34a37639f83e76433a29029d
Hsnico <59398077+Hsnico@users.noreply.github.com>
2022-05-27 01:14:40 +0800

    Update studyol.meta.js

--- files/studyol.meta.js
+++ files/studyol.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         ÔÚÏßÑ§Ï° 2.0£¡
+// @name         åœ¨çº¿å­¦ä¹  2.0ï¼
 // @namespace    http://tampermonkey.net/
 // @version      2.6.2
-// @description  ßÙßÙßÙ
-// @author       ÊÇË­ÄØ
+// @description  å“”å“”å“”
+// @author       æ˜¯è°å‘¢
 // @icon         https://www.google.com/s2/favicons?domain=jsnu.edu.cn
 // @match        *://cgjx.jsnu.edu.cn/learnspace/learn/learn/blue/*
-// @updateURL       https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.meta.js
+// @updateURL    https://hsnico.github.io/files/studyol.meta.js
 // @downloadURL  https://hsnico.github.io/files/studyol.user.js
 // @grant        none
 // ==/UserScript==
