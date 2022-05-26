3e738eb9306b0c254d32b5d75dcfed932e8c639a
Hsnico <59398077+Hsnico@users.noreply.github.com>
2022-05-27 01:12:48 +0800

    Add files via upload

---
+++ files/studyol.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         在线学习 2.0！
+// @namespace    http://tampermonkey.net/
+// @version      2.6.2
+// @description  哔哔哔
+// @author       是谁呢
+// @icon         https://www.google.com/s2/favicons?domain=jsnu.edu.cn
+// @match        *://cgjx.jsnu.edu.cn/learnspace/learn/learn/blue/*
+// @updateURL       https://tiansh.github.io/yawf/Yet_Another_Weibo_Filter.meta.js
+// @downloadURL  https://hsnico.github.io/files/studyol.user.js
+// @grant        none
+// ==/UserScript==
