546ce1d5468b183936c5019a0b3ef12ff3053a9a
Hsnico <59398077+Hsnico@users.noreply.github.com>
2022-05-28 00:39:16 +0800

    Update studyol.meta.js

--- files/studyol.meta.js
+++ files/studyol.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         在线学习 2.0！
 // @namespace    http://tampermonkey.net/
-// @version      2.6.2
+// @version      2.6.3
 // @description  哔哔哔
 // @author       是谁呢
 // @icon         https://www.google.com/s2/favicons?domain=jsnu.edu.cn
